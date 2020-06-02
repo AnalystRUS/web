25ba7d4abcc6e6106a4cbadf80d3476da7bb4fbc
AnalystRUS <deniska9434@bk.ru>
2020-06-02 19:21:47 +0300

    1_8_12

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
+#Created a symbolic test.conf reference that references nginx.conf
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+#Delete dirrectory
 sudo rm -rf /etc/nginx/sites-enabled/default
+#Restart nginx
 sudo /etc/init.d/nginx restart

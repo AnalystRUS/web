1bc5cb24239696cf8b9dece8df89e20ba2e497ee
AnalystRUS <deniska9434@bk.ru>
2020-06-21 17:18:43 +0300

    Gunicorn

--- init.sh
+++ init.sh
@@ -4,3 +4,9 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 #Restart nginx
 sudo /etc/init.d/nginx restart
+
+#***Create gunicorn configuration***
+
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+#Restart Gunicorn
+sudo /etc/init.d/gunicorn restart
